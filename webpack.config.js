c03154f7020ac7bc07c2d26810e30af99e7ff17c
monicatevy <monicasen.nk@gmail.com>
2024-02-10 01:49:08 +0100

    install bootstrap + sass & delete .idea files

--- webpack.config.js
+++ webpack.config.js
@@ -18,10 +18,11 @@ Encore
      * ENTRY CONFIG
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addStyleEntry('dashboard_user', './assets/styles/user/dashboard/styles.css')
+    .addStyleEntry('user', './assets/styles/user/dashboard/styles.scss')
+    .addStyleEntry('guest', './assets/styles/guest/home/styles.scss')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -55,7 +56,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
