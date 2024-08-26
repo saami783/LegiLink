9056ec6df582c0d87c4d485a9e8e9fe133599978
Sami Bahij <sami.bahij1@gmail.com>
2024-08-26 22:48:05 +0200

    Ajout de deux actions pour la visibilité d'une question

--- src/Controller/Admin/FaqCrudController.php
+++ src/Controller/Admin/FaqCrudController.php
@@ -3,13 +3,19 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Faq;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use GuzzleHttp\Psr7\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class FaqCrudController extends AbstractCrudController
 {
@@ -23,8 +29,8 @@ class FaqCrudController extends AbstractCrudController
     {
         return [
             IdField::new('id')->hideOnForm(),
-            TextField::new('title','Titre de la question'),
-            TextEditorField::new('content', 'Contenu de réponse')->onlyOnForms(),
+            TextField::new('question','Titre de la question'),
+            TextEditorField::new('answer', 'Contenu de réponse')->onlyOnForms(),
             TextField::new('content', 'Contenu de réponse')->renderAsHtml()->hideOnForm()->hideOnIndex(),
             BooleanField::new('isVisible', 'Est visible')->renderAsSwitch(false)
         ];
@@ -32,8 +38,70 @@ class FaqCrudController extends AbstractCrudController
 
     public function configureActions(Actions $actions): Actions
     {
-       return $actions->add(Crud::PAGE_INDEX, Crud::PAGE_DETAIL);
+        $disableFaq = function (Faq $faq) {
+            return $faq->isIsVisible();
+        };
+
+        $enableFaq = function (Faq $faq) {
+            return !$faq->isIsVisible();
+        };
+
+        $viewEnableAction = Action::new('enable', 'Rendre visible la question')
+            ->setCssClass('btn')
+            ->addCssClass('text-success')
+            ->displayIf($enableFaq)
+            ->displayAsLink()
+            ->linkToCrudAction('enable')
+            ;
+
+        $viewDisableAction = Action::new('disable', 'Désactiver la question')
+            ->setCssClass('btn')
+            ->addCssClass('text-danger')
+            ->displayIf($disableFaq)
+            ->displayAsLink()
+            ->linkToCrudAction('disable')
+            ;
+
+        $actions->add(Crud::PAGE_INDEX, Action::DETAIL);
+        $actions->add(Crud::PAGE_DETAIL, $viewEnableAction);
+        $actions->add(Crud::PAGE_INDEX, $viewEnableAction);
+
+        $actions->add(Crud::PAGE_DETAIL, $viewDisableAction);
+        $actions->add(Crud::PAGE_INDEX, $viewDisableAction);
+
+       return $actions;
     }
 
+    public function enable(AdminContext $context, AdminUrlGenerator $adminUrlGenerator, EntityManagerInterface $entityManager) : Response
+    {
+        return $this->updateFaq($context, $adminUrlGenerator, $entityManager);
+    }
 
+    public function disable(AdminContext $context, AdminUrlGenerator $adminUrlGenerator, EntityManagerInterface $entityManager) : Response
+    {
+      return $this->updateFaq($context, $adminUrlGenerator, $entityManager);
+    }
+
+    private function updateFaq(AdminContext             $context,
+                               AdminUrlGenerator        $adminUrlGenerator,
+                               EntityManagerInterface   $entityManager) : Response {
+        $faq = $context->getEntity()->getInstance();
+
+        if($faq->isIsVisible()) {
+            $faq->setIsVisible(false);
+        } else {
+            $faq->setIsVisible(true);
+        }
+
+        $entityManager->persist($faq); $entityManager->flush();
+
+        $this->addFlash('success', 'La FAQ a été désactivée avec succès.');
+
+        $url = $adminUrlGenerator
+            ->setController(self::class)
+            ->setAction(Crud::PAGE_INDEX)
+            ->generateUrl();
+
+        return $this->redirect($url);
+    }
 }
